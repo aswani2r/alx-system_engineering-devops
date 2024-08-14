aaec867742d1d357586cba6df63fc2abd12f76e5
aswani2r <ruudaswani26@gmail.com>
2024-08-14 07:07:40 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Reddit API Client"}
+    headers = {'User-Agent':'python3:subreddit.subscriber.counter:v1.0 (by /u/your_username)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        return data.get("data", {}).get("subscribers")
     else:
         return 0
+except requests.RequestException:
+	return 0
