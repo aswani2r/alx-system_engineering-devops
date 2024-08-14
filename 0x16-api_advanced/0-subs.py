468f818c7219b675bfdcaa3086bd0708c419b644
aswani2r <ruudaswani26@gmail.com>
2024-08-14 07:16:54 +0000

    task 0 api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        return data.get("data", {}).get("subscribers")
+        return data.get("data", {}).get("subscribers", 0)
     else:
         return 0
 except requests.RequestException:
